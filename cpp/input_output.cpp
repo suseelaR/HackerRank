806b82a516ffac9390a0b85387b1491b3559901c
Suseela <suseela230304@gmail.com>
2026-02-24 20:36:44 +0530

    Create input_output.cpp

---
+++ cpp/input_output.cpp
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    
+    int num1,num2,num3,sum;
+ cin>>num1>>num2>>num3;
+ sum=num1+num2+num3;
+ cout<<sum<<endl;
+    return 0;
+}

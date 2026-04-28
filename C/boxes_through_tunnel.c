7641a7fe8770f6fc41ce4113d35ebe137e233477
Suseela <suseela230304@gmail.com>
2026-04-28 22:18:45 +0530

    boxes_through_tunnel.c

---
+++ C/boxes_through_tunnel.c
@@ -0,0 +1,25 @@
+
+
+struct box
+{ int length,width,height;
+	/**
+	* Define three fields of type int: length, width and height
+	*/
+};
+
+typedef struct box box;
+
+int get_volume(box b) {
+	/**
+	* Return the volume of the box
+	*/
+ return b.length * b.height * b.width;
+}
+
+int is_lower_than_max_height(box b) {
+	/**
+	* Return 1 if the box's height is lower than MAX_HEIGHT and 0 otherwise
+	*/
+return b.height < 41 ? 1 : 0;
+}
+
